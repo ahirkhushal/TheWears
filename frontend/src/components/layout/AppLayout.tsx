51cdf19087a754d59ddaace8328fad677e883a33
khushal <ahirkhushal559@gmail.com>
2024-10-12 17:34:10 +0530

    fix-navbar-changed

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
-import { Sidebar } from "../specific/Sidebar";
-import { cn } from "@/lib/utils";
 import { useSidebar } from "@/hooks/useSidebar";
 import { useStore } from "@/hooks/useStore";
 import Footer from "./Footer";
@@ -9,18 +7,10 @@ import Footer from "./Footer";
 export default function AppLayout() {
   const sidebar = useStore(useSidebar, (x) => x);
   if (!sidebar) return null;
-  const { getOpenState, settings } = sidebar;
 
   return (
-    <main
-      className={cn(
-        "min-h-[calc(100vh_-_56px)] bg-zinc-50 transition-all duration-300 ease-in-out dark:bg-zinc-900",
-        !settings.disabled &&
-          (!getOpenState() ? "lg:ml-[90px]" : "lg:ml-[15rem]"),
-      )}
-    >
-      <Sidebar />
-      <Header title="Home" />
+    <main>
+      <Header />
       <div className="flex flex-1 justify-center">
         <main className="mx-auto max-w-7xl flex-1 py-4 sm:px-6 lg:px-8">
           <Outlet />
