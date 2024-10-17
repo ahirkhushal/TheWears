65d3e0a8ebff8f23edefd4e14a53bb697170c6c0
ahirkhushal <ahirkhushal559@gmail.com>
2024-10-17 19:16:16 +0530

    added cors rule

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://the-wears.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
